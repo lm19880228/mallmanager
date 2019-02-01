7eb508e8f346753fb4e3ef4f21b25cf23cc8136f
liming <1067869312@qq.com>
2019-02-01 17:20:57 +0800

    登录成功-保存后端返回token值

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -30,54 +30,54 @@ export default {
       // 希望让异步操作的代码看起来像同步代码 ES7写法:async+await
       // res就是 .then()方法里面的后端返回值
       const res = await this.$http.post("login", this.formdata)
-        // console.log(res.data);
-        const {
-          // 把等号右侧数据中键名为data的值赋值给data
-          data,
-          // 去等号右侧数据中找到meta的值
-          meta: { msg, status }
-        } = res.data;
-        if (status === 200) {
-          // 登陆成功 跳转home
-          this.$router.push({ name: "home" });
-          this.$message.success(msg);
-          // 2. 提示成功
-        } else {
-          // 不成功 提示信息
-          this.$message.warning(msg);
-        }
+      console.log(res);
+      const {
+        // 把等号右侧数据中键名为data的值赋值给data
+        data,
+        // 去等号右侧数据中找到meta的值
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        // 登陆成功 保存token 跳转home 提示成功
+        // localStorage.setItem('key',value) 将后台返回token字符串存入本地存储
+        localStorage.setItem('token',data.token)
+        this.$router.push({ name: "home" });
+        this.$message.success(msg);
+        
+      } else {
+        // 不成功 提示信息
+        this.$message.warning(msg);
+      }
+    }
 
+    // 正常写法:
+    // this.$http.post("login", this.formdata).then(res => {
+    //   console.log(res.data);
+    //   const {
+    //     // 把等号右侧数据中键名为data的值赋值给data
+    //     data,
+    //     // 去等号右侧数据中找到meta的值
+    //     meta: { msg, status }
+    //   } = res.data; // data: {id: 500, rid: 30, username: "admin", mobile: "12345678", email: "adsfad@qq.com", …}
+    //                 // meta: {msg: "登录成功", status: 200}
+    //   // const {meta:obj} = {meta:{msg:'登陆成功',status:200}}
+    //   // const obj = {msg:'登陆成功',status:200}
+    //   // 只提取等号右侧数据中一部分的数据
+    //   // const {meta:{msg,status}} = {meta:{msg:'登陆成功',status:200}}
+    //   // const {msg,status} = {msg:'登陆成功',status:200}
+    //   if (status === 200) {
+    //     // 登陆成功 跳转home
+    //     this.$router.push({name:'home'})
+    //     this.$message.success(msg);
 
+    //     // 2. 提示成功
+    //    }else{
+    //       // 不成功 提示信息
+    //    this.$message.warning(msg);
+    //    this.formdata = ''
+    //   }
 
-      // 正常写法:
-      // this.$http.post("login", this.formdata).then(res => {
-      //   console.log(res.data);
-      //   const {
-      //     // 把等号右侧数据中键名为data的值赋值给data
-      //     data,
-      //     // 去等号右侧数据中找到meta的值
-      //     meta: { msg, status }
-      //   } = res.data; // data: {id: 500, rid: 30, username: "admin", mobile: "12345678", email: "adsfad@qq.com", …}
-      //                 // meta: {msg: "登录成功", status: 200}
-      //   // const {meta:obj} = {meta:{msg:'登陆成功',status:200}}
-      //   // const obj = {msg:'登陆成功',status:200}
-      //   // 只提取等号右侧数据中一部分的数据
-      //   // const {meta:{msg,status}} = {meta:{msg:'登陆成功',status:200}}
-      //   // const {msg,status} = {msg:'登陆成功',status:200}
-      //   if (status === 200) {
-      //     // 登陆成功 跳转home
-      //     this.$router.push({name:'home'})
-      //     this.$message.success(msg);
-
-      //     // 2. 提示成功
-      //    }else{
-      //       // 不成功 提示信息
-      //    this.$message.warning(msg);
-      //    this.formdata = ''
-      //   }
-
-      // });
-
+    // });
   }
 };
 </script>
